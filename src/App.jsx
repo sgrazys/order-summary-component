5bfb6f2b8960ddde63ae7ed651dac4b5ae224ed7
sgrazys <s.grazys@gmail.com>
2023-08-18 21:33:52 +0300

    add styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,36 +2,54 @@ import './App.scss';
 
 function App() {
 	return (
-		<div className='card'>
-			<header className='header'></header>
-			<main className='main'>
-				<h1 className='card__heading'>Order Summary</h1>
-				<p className='card__content'>
-					You can now listen to millions of songs, audiobooks, and
-					podcasts on any device anywhere you like!
-				</p>
-				<div className='details'>
-					<div className='details__icon'></div>
-					<div className='details__pricing-box'>
-						<h2 className='title'>Annual Plan</h2>
-						<p className='price'>$59.99/year</p>
+		<>
+			<div className='card'>
+				<header className='header'></header>
+				<main className='main'>
+					<h1 className='card__heading'>Order Summary</h1>
+					<p className='card__content'>
+						You can now listen to millions of songs, audiobooks, and
+						podcasts on any device anywhere you like!
+					</p>
+					<div className='details'>
+						<div className='details__icon'></div>
+						<div className='details__pricing-box'>
+							<h2 className='title'>Annual Plan</h2>
+							<p className='price'>$59.99/year</p>
+						</div>
+
+						<a
+							href='#'
+							className='details__link'
+						>
+							Change
+						</a>
 					</div>
+					<button className='card__btn'>Proceed to Payment</button>
+
 					<a
 						href=''
-						className='details__link'
+						className='card__cancel'
 					>
-						Change
+						Cancel order
 					</a>
-				</div>
-				<button className='card__btn'>Proceed to Payment</button>
+				</main>
+			</div>
+			<div class='attribution'>
+				Challenge by{' '}
 				<a
-					href=''
-					className='card__cancel'
+					href='https://www.frontendmentor.io?ref=challenge'
+					target='_blank'
 				>
-					Cancel order
+					Frontend Mentor
 				</a>
-			</main>
-		</div>
+				. Coded by{' '}
+				<a href='https://www.linkedin.com/in/saulius-grazys/'>
+					Saulius Grazys
+				</a>
+				.
+			</div>
+		</>
 	);
 }
 
